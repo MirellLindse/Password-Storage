116f7f6dad257bd55a363fff14220661e2a60da6
MirellLindse <adrpl381@gmail.com>
2025-04-21 17:50:23 +0300

    Started working on password Auth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QInputDialog, QMessageBox
 import sys
 from gui_generation import PasswordGeneratorWidget
 from gui_storage import PasswordStorageWidget
@@ -8,7 +8,7 @@ class MainWindow(QWidget):
         super().__init__()
 
         self.setWindowTitle("PSAGA")
-        self.setFixedSize(680, 600)
+        self.setFixedSize(680, 530)
         main_layout = QVBoxLayout()
         self.password_widget = PasswordGeneratorWidget()
         self.storage_widget = PasswordStorageWidget()
@@ -18,8 +18,19 @@ class MainWindow(QWidget):
 
         self.setLayout(main_layout)
 
+
+def password_input():
+        text, ok = QInputDialog.getText(None, "Авторизация", "Введите пароль:")
+        if ok:
+            if text:
+                return True, text
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+    result = password_input()
+    if result and result[1]:
+        window = MainWindow()
+        window.show()
+        print(result)
+        sys.exit(app.exec())
+        
